de38b26811248f978a16a93a97137c5fed2a68eb
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2019-07-19 14:47:50 +0100

    Initial commit.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [0.9] (19-Jul-2019)
+### Added
+- Initial version.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Hello, Workspace</h1>
+        <p className="subtitle">Manage your files.</p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
